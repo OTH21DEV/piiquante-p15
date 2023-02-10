16929444d704645d937b79706d8987ad42d34b23
Oxana Theis <otheis@protonmail.com>
2023-02-10 10:56:44 +0000

    test

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
-const multer = require("../middleware/multer-config");
-
-router.post("/", auth, multer, sauceCtrl.createSauce);
-router.post("/:id/like", auth, multer, sauceCtrl.sauceNotation);
-router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+// const multer = require("../middleware/multer-config");
+const upload = require("../middleware/multer-config");
+router.post("/", auth, upload, sauceCtrl.createSauce);
+router.post("/:id/like", auth, upload, sauceCtrl.sauceNotation);
+router.put("/:id", auth, upload, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getAllSauces);
