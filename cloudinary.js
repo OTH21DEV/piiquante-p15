a48ba2585c6891d24342de0a127753359cbb6090
Oxana Theis <otheis@protonmail.com>
2023-02-10 13:03:49 +0000

    test

--- cloudinary.js
+++ cloudinary.js
@@ -3,9 +3,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 
-module.exports = cloudinary.config({
+cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
+module.exports = cloudinary
