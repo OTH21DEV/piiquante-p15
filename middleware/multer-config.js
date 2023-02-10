197b6783060438a9e59e70d2aba5c7c73f6ef35e
Oxana Theis <otheis@protonmail.com>
2023-02-10 16:48:52 +0000

    test cloudinary

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -32,7 +32,7 @@ const storage = multer.diskStorage({
 // module.exports = multer({storage}).single('image')
 
 module.exports = multer({
-  storage: multer.diskStorage({}).single('image'),
+  storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
--- routes/sauce.js
+++ routes/sauce.js
@@ -6,9 +6,9 @@ const auth = require("../middleware/auth");
 const upload = require("../middleware/multer-config");
 
 
-router.post("/", auth, upload, sauceCtrl.createSauce);
+router.post("/", auth, upload.single('image'), sauceCtrl.createSauce);
 router.post("/:id/like", auth, sauceCtrl.sauceNotation);
-router.put("/:id", auth, upload, sauceCtrl.modifySauce);
+router.put("/:id", auth, upload.single('image'), sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getAllSauces);
