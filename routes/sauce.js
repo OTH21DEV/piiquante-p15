097f276c01b8bab36c8af2eb8e80bab5cf647e7f
Oxana Theis <otheis@protonmail.com>
2023-02-13 17:09:32 +0000

    test

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
-// const multer = require("../middleware/multer-config");
 const upload = require("../middleware/multer-config");
 
 
