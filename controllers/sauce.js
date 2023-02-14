d9fa8de640a1fa35f663fe42eaa744fff62a7125
Oxana Theis <otheis@protonmail.com>
2023-02-14 11:29:39 +0000

    test

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -89,7 +89,7 @@ exports.modifySauce = async (req, res, next) => {
 
     delete sauceObject._userID;
 
-    sauce = await Sauce.findByIdAndUpdate(req.params.id, sauceObject, { new: true });
+    sauce = await Sauce.findOneAndUpdate(req.params.id, sauceObject);
     res.status(200).json({ message: "Sauce modified" });
     // we check if the user is owner of the sauce Object
    // next() ;
